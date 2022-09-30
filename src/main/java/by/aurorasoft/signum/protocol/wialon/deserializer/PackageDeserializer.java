e5d574ba4b516652e7caaa41eda7b00f29a34430
Vlad Zuev <vladzuev.00@mail.ru>
2022-09-30 10:34:46 +0300

    change removing prefix and postfix from package

--- src/main/java/by/aurorasoft/signum/protocol/wialon/deserializer/PackageDeserializer.java
+++ src/main/java/by/aurorasoft/signum/protocol/wialon/deserializer/PackageDeserializer.java
@@ -1,16 +1,16 @@
 package by.aurorasoft.signum.protocol.wialon.deserializer;
 
-
 import by.aurorasoft.signum.protocol.wialon.model.Package;
+
 import static by.aurorasoft.signum.protocol.wialon.model.Package.PACKAGE_POSTFIX;
 
 @FunctionalInterface
 public interface PackageDeserializer {
     Package deserialize(String deserialized);
 
-    static String removePrefixAndPostfix(String source, String prefix){
-        return source
-                .replace(prefix, "")
-                .replace(PACKAGE_POSTFIX, "");
+    static String removePrefixAndPostfix(String source, String prefix) {
+        final int indexPrefixEnd = prefix.length() - 1;
+        final int indexPostfixStart = source.length() - PACKAGE_POSTFIX.length();
+        return source.substring(indexPrefixEnd + 1, indexPostfixStart);
     }
 }
