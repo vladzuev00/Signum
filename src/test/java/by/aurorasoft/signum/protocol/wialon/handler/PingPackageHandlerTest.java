3ad67a7229872e5f3d013bc0851eddb4e8cef78f
Vlad Zuev <vladzuev.00@mail.ru>
2022-10-03 12:40:40 +0300

    add test for PingPackageHandler

---
+++ src/test/java/by/aurorasoft/signum/protocol/wialon/handler/PingPackageHandlerTest.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.signum.protocol.wialon.handler;
+
+import by.aurorasoft.signum.protocol.wialon.model.Package;
+import by.aurorasoft.signum.protocol.wialon.model.PingPackage;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class PingPackageHandlerTest {
+    private final PackageHandler packageHandler;
+
+    public PingPackageHandlerTest() {
+        this.packageHandler = new PingPackageHandler(null);
+    }
+
+    @Test
+    public void handlerShouldHandlePackage() {
+        final Package givenPackage = new PingPackage();
+
+        final String actual = this.packageHandler.doHandle(givenPackage);
+        final String expected = "#AP#";
+        assertEquals(expected, actual);
+    }
+}
