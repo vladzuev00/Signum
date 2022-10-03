ae5e65faa8eaa86e00cc26af144f2e0d350fad7c
Vlad Zuev <vladzuev.00@mail.ru>
2022-10-03 12:25:45 +0300

    refactor test for FinisherPackageHandlerTest

--- src/test/java/by/aurorasoft/signum/protocol/wialon/handler/FinisherPackageHandlerTest.java
+++ src/test/java/by/aurorasoft/signum/protocol/wialon/handler/FinisherPackageHandlerTest.java
@@ -1,19 +1,15 @@
 package by.aurorasoft.signum.protocol.wialon.handler;
 
-import by.aurorasoft.signum.ApplicationRunner;
 import by.aurorasoft.signum.protocol.wialon.model.Package;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {ApplicationRunner.class})
 public final class FinisherPackageHandlerTest {
 
-    @Autowired
-    private FinisherPackageHandler packageHandler;
+    private final FinisherPackageHandler packageHandler;
+
+    public FinisherPackageHandlerTest() {
+        this.packageHandler = new FinisherPackageHandler();
+    }
 
     @Test(expected = UnsupportedOperationException.class)
     public void handlerShouldNotHandlePackage() {
