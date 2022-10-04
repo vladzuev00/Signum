3c4d336cb1f531146f8377675ba49d7b7c2861fc
Vlad Zuev <vladzuev.00@mail.ru>
2022-10-04 12:47:51 +0300

    add units in entity of user

--- src/main/java/by/aurorasoft/signum/entity/UserEntity.java
+++ src/main/java/by/aurorasoft/signum/entity/UserEntity.java
@@ -3,7 +3,9 @@ package by.aurorasoft.signum.entity;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -15,4 +17,6 @@ import javax.persistence.Table;
 @Builder
 public class UserEntity extends NamedEntity {
 
+    @OneToMany(mappedBy = "user")
+    private List<UnitEntity> units;
 }
