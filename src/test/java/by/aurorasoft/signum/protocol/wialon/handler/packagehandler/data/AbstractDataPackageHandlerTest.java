17d7ea4fdc8c3df045fb0e2d24b3e25cf850f435
vladzuev <vladzuev.00@mail.ru>
2022-12-01 03:51:17 -0600

    refactor test in AbstractDataPackageHandlerTests

--- src/test/java/by/aurorasoft/signum/protocol/wialon/handler/packagehandler/data/AbstractDataPackageHandlerTest.java
+++ src/test/java/by/aurorasoft/signum/protocol/wialon/handler/packagehandler/data/AbstractDataPackageHandlerTest.java
@@ -1,13 +1,11 @@
 package by.aurorasoft.signum.protocol.wialon.handler.packagehandler.data;
 
-import by.aurorasoft.signum.crud.model.dto.Device;
 import by.aurorasoft.signum.crud.model.dto.Message;
 import by.aurorasoft.signum.crud.model.dto.Message.GpsCoordinate;
-import by.aurorasoft.signum.protocol.core.contextmanager.ContextManager;
+import by.aurorasoft.signum.protocol.core.service.receivemessage.ReceivingMessageService;
 import by.aurorasoft.signum.protocol.wialon.model.AbstractDataPackage;
-import by.aurorasoft.signum.protocol.wialon.model.Package;
-import by.aurorasoft.signum.crud.service.MessageService;
 
+import by.aurorasoft.signum.protocol.wialon.model.Package;
 import io.netty.channel.ChannelHandlerContext;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,100 +22,84 @@ import static java.time.Instant.now;
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 
-//@RunWith(MockitoJUnitRunner.class)
-//public final class AbstractDataPackageHandlerTest {
-//    private static final String RESPONSE_TEMPLATE = "#RESPONSE#%d";
-//
-//    @Mock
-//    private MessageService mockedService;
-//
-//    @Mock
-//    private ContextManager mockedContextManager;
-//
-//    private AbstractDataPackageHandler<HandledPackage> packageHandler;
-//
-//    @Captor
-//    private ArgumentCaptor<List<Message>> messagesArgumentCaptor;
-//
-//    @Captor
-//    private ArgumentCaptor<ChannelHandlerContext> contextArgumentCaptor;
-//
-//    @Captor
-//    private ArgumentCaptor<Long> longArgumentCaptor;
-//
-//    @Captor
-//    private ArgumentCaptor<String> stringArgumentCaptor;
-//
-//    @Before
-//    public void initializePackageHandler() {
-//        this.packageHandler = new HandledPackageHandler(this.mockedService, this.mockedContextManager);
-//    }
-//
-//    @Test
-//    @SuppressWarnings("all")
-//    public void handlerShouldHandlePackage() {
-//        final List<Message> givenMessages = List.of(createMessage(), createMessage(), createMessage());
-//        final HandledPackage givenPackage = new HandledPackage(givenMessages);
-//        final ChannelHandlerContext givenContext = mock(ChannelHandlerContext.class);
-//
-//        final Device givenDevice = mock(Device.class);
-//        final Long givenDeviceId = 255L;
-//        when(givenDevice.getId()).thenReturn(givenDeviceId);
-//
-//        when(this.mockedContextManager.findDevice(any(ChannelHandlerContext.class))).thenReturn(givenDevice);
-//
-//        final List<Message> givenSavedMessages = mock(List.class);
-//        final int givenAmountSavedMessages = givenMessages.size();
-//        when(givenSavedMessages.size()).thenReturn(givenAmountSavedMessages);
-//        when(this.mockedService.saveAll(anyLong(), anyList())).thenReturn(givenSavedMessages);
-//
-//        this.packageHandler.doHandle(givenPackage, givenContext);
-//
-//        verify(this.mockedContextManager, times(1))
-//                .findDevice(this.contextArgumentCaptor.capture());
-//        verify(this.mockedService, times(1))
-//                .saveAll(this.longArgumentCaptor.capture(), this.messagesArgumentCaptor.capture());
-//        verify(givenContext, times(1))
-//                .writeAndFlush(this.stringArgumentCaptor.capture());
-//
-//        assertSame(givenContext, this.contextArgumentCaptor.getValue());
-//        assertEquals(givenDeviceId, this.longArgumentCaptor.getValue());
-//        assertSame(givenMessages, this.messagesArgumentCaptor.getValue());
-//        assertEquals("#RESPONSE#3", this.stringArgumentCaptor.getValue());
-//    }
-//
-//    @Test(expected = ClassCastException.class)
-//    public void handlerShouldNotHandlePackageOfNotSuitableType() {
-//        final Package givenPackage = new Package() {
-//        };
-//        final ChannelHandlerContext givenContext = mock(ChannelHandlerContext.class);
-//        this.packageHandler.doHandle(givenPackage, givenContext);
-//    }
-//
-//    @SuppressWarnings("all")
-//    private static final class HandledPackage extends AbstractDataPackage {
-//        public HandledPackage(List<Message> messages) {
-//            super(messages);
-//        }
-//    }
-//
-//    @SuppressWarnings("all")
-//    private static final class HandledPackageHandler extends AbstractDataPackageHandler<HandledPackage> {
-//        public HandledPackageHandler(MessageService messageService, ContextManager contextWorker) {
-//            super(HandledPackage.class, null, messageService, contextWorker);
-//        }
-//
-//        @Override
-//        protected String createResponse(int amountSavedMessages) {
-//            return format(RESPONSE_TEMPLATE, amountSavedMessages);
-//        }
-//    }
-//
-//    private static Message createMessage() {
-//        return new Message(now(), new GpsCoordinate(1, 2), 3, 4, 5, 6,
-//                emptyMap());
-//    }
-//}
+@RunWith(MockitoJUnitRunner.class)
+public final class AbstractDataPackageHandlerTest {
+    private static final String RESPONSE_TEMPLATE = "#RESPONSE#%d";
+
+    @Mock
+    private ReceivingMessageService mockedReceivingMessageService;
+
+    private AbstractDataPackageHandler<HandledPackage> packageHandler;
+
+    @Captor
+    private ArgumentCaptor<List<Message>> messagesArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<ChannelHandlerContext> contextArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Before
+    public void initializePackageHandler() {
+        this.packageHandler = new HandledPackageHandler(this.mockedReceivingMessageService);
+    }
+
+    @Test
+    public void handlerShouldHandlePackage() {
+        final List<Message> givenMessages = List.of(createMessage(), createMessage(), createMessage());
+        final HandledPackage givenPackage = new HandledPackage(givenMessages);
+        final ChannelHandlerContext givenContext = mock(ChannelHandlerContext.class);
+
+        this.packageHandler.doHandle(givenPackage, givenContext);
+
+        verify(this.mockedReceivingMessageService, times(1))
+                .receive(this.contextArgumentCaptor.capture(), this.messagesArgumentCaptor.capture());
+        verify(givenContext, times(1)).writeAndFlush(this.stringArgumentCaptor.capture());
+
+        assertSame(givenContext, this.contextArgumentCaptor.getValue());
+        assertSame(givenMessages, this.messagesArgumentCaptor.getValue());
+        assertEquals("#RESPONSE#3", this.stringArgumentCaptor.getValue());
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void handlerShouldNotHandlePackageOfNotSuitableType() {
+        final Package givenPackage = new Package() {
+        };
+        final ChannelHandlerContext givenContext = mock(ChannelHandlerContext.class);
+        this.packageHandler.doHandle(givenPackage, givenContext);
+    }
+
+    @SuppressWarnings("all")
+    private static final class HandledPackage extends AbstractDataPackage {
+        public HandledPackage(List<Message> messages) {
+            super(messages);
+        }
+    }
+
+    @SuppressWarnings("all")
+    private static final class HandledPackageHandler extends AbstractDataPackageHandler<HandledPackage> {
+        public HandledPackageHandler(ReceivingMessageService receivingMessageService) {
+            super(HandledPackage.class, null, receivingMessageService);
+        }
+
+        @Override
+        protected String createResponse(int amountSavedMessages) {
+            return format(RESPONSE_TEMPLATE, amountSavedMessages);
+        }
+    }
+
+    private static Message createMessage() {
+        return Message.builder()
+                .datetime(now())
+                .coordinate(new GpsCoordinate(1, 2))
+                .speed(3)
+                .course(4)
+                .altitude(6)
+                .amountSatellite(6)
+                .parameterNamesByValues(emptyMap())
+                .build();
+    }
+}
