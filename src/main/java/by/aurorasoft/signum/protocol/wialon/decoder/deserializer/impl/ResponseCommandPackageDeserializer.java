93f7ac73c11562287df67f09f9f761e5f0be2508
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-26 13:13:06 +0300

    refactor ResponseCommandPackageDeserializer

--- src/main/java/by/aurorasoft/signum/protocol/wialon/decoder/deserializer/impl/ResponseCommandPackageDeserializer.java
+++ src/main/java/by/aurorasoft/signum/protocol/wialon/decoder/deserializer/impl/ResponseCommandPackageDeserializer.java
@@ -1,24 +1,30 @@
 package by.aurorasoft.signum.protocol.wialon.decoder.deserializer.impl;
 
 import by.aurorasoft.signum.protocol.wialon.decoder.deserializer.PackageDeserializer;
+import by.aurorasoft.signum.protocol.wialon.model.Package;
 import by.aurorasoft.signum.protocol.wialon.model.ResponseCommandPackage;
 import by.aurorasoft.signum.protocol.wialon.model.ResponseCommandPackage.Status;
-
 import org.springframework.stereotype.Component;
 
-import static by.aurorasoft.signum.protocol.wialon.decoder.deserializer.PackageDeserializer.removePrefix;
 import static by.aurorasoft.signum.protocol.wialon.model.ResponseCommandPackage.PACKAGE_PREFIX;
 import static by.aurorasoft.signum.protocol.wialon.model.ResponseCommandPackage.Status.findByValue;
 import static java.lang.Byte.parseByte;
 
 @Component
-public final class ResponseCommandPackageDeserializer implements PackageDeserializer {
+public final class ResponseCommandPackageDeserializer extends PackageDeserializer {
+
+    public ResponseCommandPackageDeserializer() {
+        super(PACKAGE_PREFIX);
+    }
 
     @Override
-    public ResponseCommandPackage deserialize(String deserialized) {
-        final String serializedStatusValue = removePrefix(deserialized, PACKAGE_PREFIX);
-        final byte statusValue = parseByte(serializedStatusValue);
-        final Status status = findByValue(statusValue);
+    protected Package deserializeMessage(final String message) {
+        final Status status = findCommandStatus(message);
         return new ResponseCommandPackage(status);
+    }
+
+    private static Status findCommandStatus(final String message) {
+        final byte statusValue = parseByte(message);
+        return findByValue(statusValue);
     }
 }
