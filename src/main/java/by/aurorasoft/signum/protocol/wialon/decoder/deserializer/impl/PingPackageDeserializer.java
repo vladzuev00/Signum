56c407e70bd5162c94573f6f2abc5660c205e41d
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-26 13:08:25 +0300

    refactor PingPackageDeserializer

--- src/main/java/by/aurorasoft/signum/protocol/wialon/decoder/deserializer/impl/PingPackageDeserializer.java
+++ src/main/java/by/aurorasoft/signum/protocol/wialon/decoder/deserializer/impl/PingPackageDeserializer.java
@@ -1,14 +1,21 @@
 package by.aurorasoft.signum.protocol.wialon.decoder.deserializer.impl;
 
 import by.aurorasoft.signum.protocol.wialon.decoder.deserializer.PackageDeserializer;
+import by.aurorasoft.signum.protocol.wialon.model.Package;
 import by.aurorasoft.signum.protocol.wialon.model.PingPackage;
 import org.springframework.stereotype.Component;
 
+import static by.aurorasoft.signum.protocol.wialon.model.PingPackage.PACKAGE_DESCRIPTION_PREFIX;
+
 @Component
-public final class PingPackageDeserializer implements PackageDeserializer {
+public final class PingPackageDeserializer extends PackageDeserializer {
+
+    public PingPackageDeserializer() {
+        super(PACKAGE_DESCRIPTION_PREFIX);
+    }
 
     @Override
-    public PingPackage deserialize(String deserialized) {
+    protected Package deserializeMessage(final String message) {
         return new PingPackage();
     }
 }
